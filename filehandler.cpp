319d1e8a337708c33f6a368eac82a3904fafffda
NotCB <129556805+NotCB@users.noreply.github.com>
2023-04-01 12:40:10 +1000

    Split filehandler function into multiple functions.

--- filehandler.cpp
+++ filehandler.cpp
@@ -3,30 +3,45 @@
 #include <iostream>
 #include <string>
 
-int filehandler(std::string fn,std::string ui){
+int createfile(std::string fn){
 	std::fstream fs;
-	std::string line="init";
-	std::transform(ui.begin(),ui.end(),ui.begin(),[](unsigned char c){return std::tolower(c);});//ui tolowercase
-	if (ui=="a"||ui=="w"||ui=="write"){
-		fs.open(fn,std::ios::out|std::ios::app);
-		if(!fs.is_open()){std::cout<<"Error! Unable to write to "<<fn<<"!\n";return 1;}
-		while(line!=""){getline(std::cin,line);fs<<line<<'\n';}
-	}
-	else if(ui=="c"||ui=="clear"||ui=="create"){
-		fs.open(fn,std::ios::out|std::ios::trunc);
-		if(!fs.is_open()){std::cout<<"Error!\n";return 1;}
-	}
-	else if(ui=="r"||ui=="read"){
-		fs.open(fn,std::ios::in);
-		if(!fs.is_open()){std::cout<<"Error! Unable to open "<<fn<<"!\n";return 1;}
-		else{
-			while(getline(fs,line)){
-				std::cout<<line<<'\n';
-			}
+	fs.open(fn,std::ios::out|std::ios::trunc);
+	if(!fs.is_open()){std::cout<<"Error!\n";return 1;}
+fs.close();return 0;}
+
+int readfile(std::string fn){
+	std::string line;
+	std::fstream fs;
+	fs.open(fn,std::ios::in);
+	if(!fs.is_open()){std::cout<<"Error! Unable to open "<<fn<<"!\n";return 1;}
+	else{
+		while(getline(fs,line)){
+			std::cout<<line<<'\n';
 		}
 	}
-	else{return 1;}fs.close();return 0;
-}
+fs.close();return 0;}
+
+int writefile(std::string fn){
+	std::string line="init";
+	std::fstream fs;
+	fs.open(fn,std::ios::out|std::ios::app);
+	if(!fs.is_open()){std::cout<<"Error! Unable to write to "<<fn<<"!\n";return 1;}
+	while(line!=""){getline(std::cin,line);fs<<line<<'\n';}
+fs.close();return 0;}
+
+int filehandler(std::string fn,std::string ui){
+	std::transform(ui.begin(),ui.end(),ui.begin(),[](unsigned char c){return std::tolower(c);});//ui tolowercase
+	if (ui=="a"||ui=="w"||ui=="write"){
+		return writefile(fn);
+	}
+	else if(ui=="c"||ui=="clear"||ui=="create"){
+		return createfile(fn);
+	}
+	else if(ui=="r"||ui=="read"){
+		return readfile(fn);
+	}
+	else{return 1;}
+return 1;}
 
 int main(int argc, char *argv[]){
 	//Variables
@@ -53,4 +68,4 @@ int main(int argc, char *argv[]){
 		std::getline(std::cin,userinput);
 		filehandler(filenames[i],userinput);
 	}
-}
+return 0;}
