b6fb3364d1da4f4078b91d5c331e2663c50ce710
NotCB <129556805+NotCB@users.noreply.github.com>
2023-04-02 18:56:10 +1000

    Delete clearlog.cpp

--- clearlog.cpp
+++
@@ -1,8 +0,0 @@
-#include <fstream>
-
-int main(){
-	std::fstream a("alltime.log",std::ios::out|std::ios::trunc);
-	a.close();
-	std::fstream e("error.log",std::ios::out|std::ios::trunc);
-	e.close();
-return 0;}
