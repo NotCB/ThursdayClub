f6bf623bcd223bca2c401a4858be9660e91a5002
NotCB <129556805+NotCB@users.noreply.github.com>
2023-04-14 20:12:00 +1000

    Textures

--- opengl/learnopengl_com/main.cpp
+++ opengl/learnopengl_com/main.cpp
@@ -2,8 +2,10 @@
 
 #include <glad/glad.h> //include glad before GLFW
 #include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 
-#include "eout.h"
+#include "filesystem.h"
 #include "shader_s.h"
 
 void framebuffer_size_callback(GLFWwindow* window,int width, int height);
@@ -36,19 +38,22 @@ int main(){
 	Shader ourShader("vs.glsl","fs.glsl");
 	
 	float vertices[]={
-		-0.5f,-0.5f,0.0f,1.0f,0.0f,0.0f,
-		0.5f,-0.5f,0.0f,0.0f,1.0f,0.0f,
-		0.0f,0.5f,0.0f,0.0f,0.0f,1.0f
+		0.5f,0.5f,0.0f,1.0f,1.0f,1.0f,1.0f,1.0f,
+		0.5f,-0.5f,0.0f,1.0f,1.0f,1.0f,1.0f,0.0f,
+		-0.5f,-0.5f,0.0f,1.0f,1.0f,1.0f,0.0f,0.0f,
+		-0.5f,0.5f,0.0f,1.0f,1.0f,1.0f,0.0f,1.0f
 	};
 	
 	unsigned int indices[]={
-		0,1,2
+		0,1,3,
+		1,2,3
 	};
 	
 	unsigned int VAO,VBO,EBO;
 	glGenVertexArrays(1,&VAO);
 	glGenBuffers(1,&VBO);
 	glGenBuffers(1,&EBO);
+	
 	glBindVertexArray(VAO);
 	
 	glBindBuffer(GL_ARRAY_BUFFER,VBO);
@@ -57,11 +62,52 @@ int main(){
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
 	
-	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)0);
+	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,8*sizeof(float),(void*)0);
 	glEnableVertexAttribArray(0);
 	
-	glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)(3*sizeof(float)));
+	glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,8*sizeof(float),(void*)(3*sizeof(float)));
 	glEnableVertexAttribArray(1);
+	
+	glVertexAttribPointer(2,2,GL_FLOAT,GL_FALSE,8*sizeof(float),(void*)(6*sizeof(float)));
+	glEnableVertexAttribArray(2);
+	
+	unsigned int texture1,texture2;
+	int width,height,nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	
+	glGenTextures(1,&texture1);
+	glBindTexture(GL_TEXTURE_2D,texture1);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+	unsigned char *data=stbi_load(FileSystem::getPath("textures/container.jpg").c_str(),&width,&height,&nrChannels,0);
+	if(data){
+		glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,GL_UNSIGNED_BYTE,data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}else{
+		eout<<"Failed to load texture.\n";
+	}
+	stbi_image_free(data);
+	
+	glGenTextures(1,&texture2);
+	glBindTexture(GL_TEXTURE_2D,texture2);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+	data=stbi_load(FileSystem::getPath("textures/awesomeface.png").c_str(),&width,&height,&nrChannels,0);
+	if(data){
+		glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,width,height,0,GL_RGBA,GL_UNSIGNED_BYTE,data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}else{
+		eout<<"Failed to load texture.\n";
+	}
+	stbi_image_free(data);
+	
+	ourShader.use();
+	glUniform1i(glGetUniformLocation(ourShader.ID,"texture1"),0);
+	ourShader.setInt("texture2",1);
 	
 	//glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);//wireframe polygons
 	
@@ -71,9 +117,14 @@ int main(){
 		glClearColor(0.75f,0.75f,0.75f,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D,texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D,texture2);
+		
 		ourShader.use();
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES,3,GL_UNSIGNED_INT,0);
+		glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
 		
 		glfwSwapBuffers(window);
 		glfwPollEvents();
