1b71e14db8024b64aeaf50067509aed547d869a6
NotCB <129556805+NotCB@users.noreply.github.com>
2023-04-16 21:48:00 +1000

    Camera

--- opengl/learnopengl_com/main.cpp
+++ opengl/learnopengl_com/main.cpp
@@ -1,5 +1,3 @@
-#include <fstream>
-
 #include <glad/glad.h> //include glad before GLFW
 #include <GLFW/glfw3.h>
 #define STB_IMAGE_IMPLEMENTATION
@@ -9,27 +7,40 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "include/eout.h"
 #include "include/filesystem.h"
 #include "include/shader.h"
+#include "include/camera.h"
 
 void framebuffer_size_callback(GLFWwindow* window,int width, int height);
+void mouse_callback(GLFWwindow* window,double xpos,double ypos);
+void scroll_callback(GLFWwindow* window,double xoffset,double yoffset);
 void processInput(GLFWwindow *window);
 
 unsigned int loadTextureRGB(const char file[]);
 unsigned int loadTextureRGBA(const char file[]);
 
-unsigned int windowWidth=1920;
-unsigned int windowHeight=1080;
-char windowName[]="Window Name";
+unsigned int screenWidth=800;
+unsigned int screenHeight=600;
+char screenName[]="Window Name";
+
+Camera camera(glm::vec3(0.0f,0.0f,3.0f));
+float lastX=screenWidth/2.0f;
+float lastY=screenHeight/2.0f;
+bool firstMouse=true;
+
+float deltaTime=0.0f;
+float lastFrame=0.0f;
 
 int main(){
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,GL_TRUE);
-	GLFWwindow* window=glfwCreateWindow(windowWidth,windowHeight,windowName,NULL,NULL);
+#endif
+
+	GLFWwindow* window=glfwCreateWindow(screenWidth,screenHeight,screenName,NULL,NULL);
 	if(window==NULL){
 		eout<<"Failed to create GLFW window.\n";
 		glfwTerminate();
@@ -37,36 +48,89 @@ int main(){
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
+	glfwSetCursorPosCallback(window,mouse_callback);
+	glfwSetScrollCallback(window,scroll_callback);
+	
+	glfwSetInputMode(window,GLFW_CURSOR,GLFW_CURSOR_DISABLED);
+	
 	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
 		eout<<"Failed to initialize GLAD.\n";
 		return -1;
 	}
 	
+	glEnable(GL_DEPTH_TEST);
+	
 	Shader ourShader("vs.glsl","fs.glsl");
 	
-	float vertices[]={
-		-0.5f,0.5f,0.0f,0.0f,1.0f,
-		0.5f,0.5f,0.0f,1.0f,1.0f,
-		-0.5f,-0.5f,0.0f,0.0f,0.0f,
-		0.5f,-0.5f,0.0f,1.0f,0.0f
-	};
-	unsigned int indices[]={
-		0,1,2,
-		1,2,3
-	};
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+    // world space positions of our cubes
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
 	
 	unsigned int VAO,VBO,EBO;
 	glGenVertexArrays(1,&VAO);
 	glGenBuffers(1,&VBO);
-	glGenBuffers(1,&EBO);
+	//glGenBuffers(1,&EBO);
 	
 	glBindVertexArray(VAO);
 	
 	glBindBuffer(GL_ARRAY_BUFFER,VBO);
 	glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
 	
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
 	
 	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,5*sizeof(float),(void*)0);
 	glEnableVertexAttribArray(0);
@@ -86,26 +150,37 @@ int main(){
 	//glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);//wireframe polygons
 	
 	while(!glfwWindowShouldClose(window)){
+		float currentFrame=static_cast<float>(glfwGetTime());
+		deltaTime=currentFrame-lastFrame;
+		lastFrame=currentFrame;
+		
 		processInput(window);
 		
 		glClearColor(0.75f,0.75f,0.75f,1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D,texture1);
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D,texture2);
 		
-		glm::mat4 transform=glm::mat4(1.0f);
-		transform=glm::translate(transform,glm::vec3(0.5f,-0.5f,0.0f));
-		transform=glm::rotate(transform,(float)glfwGetTime(),glm::vec3(0.0f,0.0f,1.0f));
-		
 		ourShader.use();
-		unsigned int transformLoc=glGetUniformLocation(ourShader.ID,"transform");
-		glUniformMatrix4fv(transformLoc,1,GL_FALSE,glm::value_ptr(transform));
+		
+		glm::mat4 projection=glm::perspective(glm::radians(camera.Zoom),(float)screenWidth/(float)screenHeight,0.1f,100.0f);
+		ourShader.setMat4("projection",projection);
+		
+		glm::mat4 view=camera.GetViewMatrix();
+		ourShader.setMat4("view",view);
 		
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+		for(unsigned int i=0;i<10;i++){
+			glm::mat4 model=glm::mat4(1.0f);
+			model=glm::translate(model,cubePositions[i]);
+			float angle=20.0f * i;
+			model=glm::rotate(model,glm::radians(angle),glm::vec3(1.0f,0.3f,0.5f));
+			ourShader.setMat4("model",model);
+			glDrawArrays(GL_TRIANGLES,0,36);
+		}
 		
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -113,7 +188,7 @@ int main(){
 	
 	glDeleteVertexArrays(1,&VAO);
 	glDeleteBuffers(1,&VBO);
-	glDeleteBuffers(1,&EBO);
+	//glDeleteBuffers(1,&EBO);
 	
 	glfwTerminate();
 	return 0;
@@ -123,10 +198,40 @@ void processInput(GLFWwindow *window){
 	if(glfwGetKey(window,GLFW_KEY_ESCAPE)==GLFW_PRESS){
 		glfwSetWindowShouldClose(window,true);
 	}
+	
+	if(glfwGetKey(window,GLFW_KEY_W)==GLFW_PRESS){
+		camera.ProcessKeyboard(FORWARD,deltaTime);
+	}
+	if(glfwGetKey(window,GLFW_KEY_S)==GLFW_PRESS){
+		camera.ProcessKeyboard(BACKWARD,deltaTime);
+	}
+	if(glfwGetKey(window,GLFW_KEY_A)==GLFW_PRESS){
+		camera.ProcessKeyboard(LEFT,deltaTime);
+	}
+	if(glfwGetKey(window,GLFW_KEY_D)==GLFW_PRESS){
+		camera.ProcessKeyboard(RIGHT,deltaTime);
+	}
 }
 
 void framebuffer_size_callback(GLFWwindow* window,int width,int height){
 	glViewport(0,0,width,height);
+}
+
+void mouse_callback(GLFWwindow* window,double xposIn,double yposIn){
+	float xpos=static_cast<float>(xposIn);
+	float ypos=static_cast<float>(yposIn);
+	if(firstMouse){
+		lastX=xpos;lastY=ypos;
+		firstMouse=false;
+	}
+	float xoffset=xpos-lastX;
+	float yoffset=lastY-ypos;// reversed since y-coordinates go from bottom to top
+	lastX=xpos;lastY=ypos;
+	camera.ProcessMouseMovement(xoffset,yoffset);
+}
+
+void scroll_callback(GLFWwindow* window,double xoffset,double yoffset){
+	camera.ProcessMouseScroll(static_cast<float>(yoffset));
 }
 
 unsigned int loadTextureRGB(const char file[]){
