5b35a2f8a977341f9b62a1c254af2400bc7c6add
NotCB <129556805+NotCB@users.noreply.github.com>
2023-04-27 22:30:00 +1000

    Rotating Cube

---
+++ opengl/pdf-openglcpp/fShader.glsl
@@ -0,0 +1,6 @@
+#version 430
+in vec4 varyingColor;
+out vec4 color;
+void main(){
+color=varyingColor;//vec4(0.34,0.0,0.796,1.0);
+}
--- opengl/pdf-openglcpp/program.cpp
+++ opengl/pdf-openglcpp/program.cpp
@@ -1,25 +1,127 @@
-// 48 / 535
-
-#include <fstream>
-#include <string>
+// 92 / 535
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <cmath>
+#include <fstream>
+#include <string>
 
 std::fstream eout("error.log",std::ios::out);
 
-#define numVAOs 1
+class Utils{
+public:
+	GLuint createShaderProgram(const char *vp,const char *fp){
+		GLint vertCompiled,fragCompiled,linked;
+		GLuint vShader=glCreateShader(GL_VERTEX_SHADER);
+		GLuint fShader=glCreateShader(GL_FRAGMENT_SHADER);
+		std::string vertShaderStr=readShaderSource(vp);
+		std::string fragShaderStr=readShaderSource(fp);
+		const char* vertShaderSrc=vertShaderStr.c_str();
+		const char* fragShaderSrc=fragShaderStr.c_str();
+		glShaderSource(vShader,1,&vertShaderSrc,NULL);
+		glShaderSource(fShader,1,&fragShaderSrc,NULL);
+		glCompileShader(vShader);
+		checkOpenGLError();
+		glGetShaderiv(vShader,GL_COMPILE_STATUS,&vertCompiled);
+		if(vertCompiled!=1){
+			eout<<"Vertex Compilation Failed!\n";
+			printShaderLog(vShader);
+		}
+		glCompileShader(fShader);
+		checkOpenGLError();
+		glGetShaderiv(fShader,GL_COMPILE_STATUS,&fragCompiled);
+		if(fragCompiled!=1){
+			eout<<"Fragment Compilation Failed!\n";
+			printShaderLog(fShader);
+		}
+		GLuint vfProgram=glCreateProgram();
+		glAttachShader(vfProgram,vShader);
+		glAttachShader(vfProgram,fShader);
+		glLinkProgram(vfProgram);
+		checkOpenGLError();
+		glGetProgramiv(vfProgram,GL_LINK_STATUS,&linked);
+		if(linked!=1){
+			eout<<"Linking Failed!\n";
+			printProgramLog(vfProgram);
+		}
+		return vfProgram;
+	}
+	
+	//GLuint createShaderProgram(const char *vp,const char *gp,const char *fp){}
+	//GLuint createShaderProgram(const char *vp,const char *tCS,const char *tES,const char *fp){}
+	//GLuint createShaderProgram(const char *vp,const char *tCS,const char *tES,const char *gp,const char *fp){}
+	
+private:
+	std::string readShaderSource(const char* filePath){
+		std::string content;
+		std::ifstream fileStream(filePath,std::ios::in);
+		std::string line="";
+		while(!fileStream.eof()){
+			getline(fileStream,line);
+			content.append(line+'\n');
+		}
+		fileStream.close();
+		return content;
+	}
+	
+	bool checkOpenGLError(){
+		bool foundError=false;
+		int glErr=glGetError();
+		while(glErr!=GL_NO_ERROR){
+			eout<<"glError: "<<glErr<<'\n';
+			foundError=true;
+			glErr=glGetError();
+		}
+		return foundError;
+	}
+	
+	void printShaderLog(GLuint shader){
+		int len=0;
+		int chWrittn=0;
+		char* log;
+		glGetShaderiv(shader,GL_INFO_LOG_LENGTH,&len);
+		if(len>0){
+			log=(char*)malloc(len);
+			glGetShaderInfoLog(shader,len,&chWrittn,log);
+			eout<<"Shader Info Log: "<<log<<'\n';
+			free(log);
+		}
+	}
+	
+	void printProgramLog(int prog){
+		int len=0;
+		int chWrittn=0;
+		char* log;
+		glGetProgramiv(prog,GL_INFO_LOG_LENGTH,&len);
+		if(len>0){
+			log=(char*)malloc(len);
+			glGetProgramInfoLog(prog,len,&chWrittn,log);
+			eout<<"Program Info Log: "<<log<<'\n';
+			free(log);
+		}
+	}
+};
 
+#define numVAOs 1
+#define numVBOs 2
+
+float cameraX,cameraY,cameraZ;
+float cubeLocX,cubeLocY,cubeLocZ;
 GLuint renderingProgram;
 GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
+
+GLuint mvLoc,projLoc;
+int width,height;
+float aspect;
+glm::mat4 pMat,vMat,mMat,mvMat,tMat,rMat;
 
 void init(GLFWwindow* window);
 void display(GLFWwindow* window,double currentTime);
-GLuint createShaderProgram();
-void printShaderLog(GLuint shader);
-void printProgramLog(int prog);
-bool checkOpenGLError();
-std::string readShaderSource(const char* filePath);
+void setupVertices();
 
 int main(void){
 	if(!glfwInit()){exit(EXIT_FAILURE);}
@@ -37,113 +139,73 @@ int main(void){
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-	
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	exit(EXIT_SUCCESS);
 }
 
 void init(GLFWwindow* window){
-	renderingProgram=createShaderProgram();
-	glGenVertexArrays(numVAOs,vao);
-	glBindVertexArray(vao[0]);
+	Utils initUtils;
+	renderingProgram=initUtils.createShaderProgram("vShader.glsl","fShader.glsl");
+	cameraX=0.0f;cameraY=0.0f;cameraZ=8.0f;
+	cubeLocX=0.0f;cubeLocY=-2.0f;cubeLocZ=0.0f;
+	setupVertices();
 }
 
 void display(GLFWwindow* window,double currentTime){
-	//glClearColor(0.75f,0.75f,0.75f,1.0f);
-	//glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT);
 	glUseProgram(renderingProgram);
-	glDrawArrays(GL_TRIANGLES,0,3);
-}
-
-GLuint createShaderProgram(){
-	GLint vertCompiled,fragCompiled,linked;
-	GLuint vShader=glCreateShader(GL_VERTEX_SHADER);
-	GLuint fShader=glCreateShader(GL_FRAGMENT_SHADER);
 	
-	string vertShaderStr=readShaderSource("vertShader.glsl");
-	string fragShaderStr=readShaderSource("fragShader.glsl");
-	const char* vertShaderSrc=vertShader.c_str();
-	const char* fragShaderSrc=fragShader.c_str();
-	glShaderSource(vShader,1,&vertShaderSrc,NULL);
-	glShaderSource(fShader,1,&fragShaderSrc,NULL);
+	mvLoc=glGetUniformLocation(renderingProgram,"mv_matrix");
+	projLoc=glGetUniformLocation(renderingProgram,"proj_matrix");
 	
-	glCompileShader(vShader);
-	checkOpenGLError();
-	glGetShaderiv(vShader,GL_COMPILE_STATUS,&vertCompiled);
-	if(vertCompiled!=1){
-		eout<<"Vertex Compilation Failed!\n";
-		printShaderLog(vShader);
-	}
+	glfwGetFramebufferSize(window,&width,&height);
+	aspect=(float)width/(float)height;
+	pMat=glm::perspective(1.0472f,aspect,0.1f,1000.0f);//1.0472 radians = 60 degrees
 	
-	glCompileShader(fShader);
-	checkOpenGLError();
-	glGetShaderiv(fShader,GL_COMPILE_STATUS,&fragCompiled);
-	if(fragCompiled!=1){
-		eout<<"Fragment Compilation Failed!\n";
-		printShaderLog(fShader);
-	}
+	vMat=glm::translate(glm::mat4(1.0f),glm::vec3(-cameraX,-cameraY,-cameraZ));
+	mMat=glm::translate(glm::mat4(1.0f),glm::vec3(cubeLocX,cubeLocY,cubeLocZ));
 	
-	GLuint vfProgram=glCreateProgram();
-	glAttachShader(vfProgram,vShader);
-	glAttachShader(vfProgram,fShader);
+	tMat=glm::translate(glm::mat4(1.0f),glm::vec3(sin(0.35f*currentTime)*2.0f,cos(0.52f*currentTime)*2.0f,sin(0.7*currentTime)*2.0f));
+	rMat=glm::rotate(glm::mat4(1.0f),1.75f *(float)currentTime,glm::vec3(0.0f,1.0f,0.0f));
+	rMat=glm::rotate(rMat,1.75f *(float)currentTime,glm::vec3(1.0f,0.0f,0.0f));
+	rMat=glm::rotate(rMat,1.75f *(float)currentTime,glm::vec3(0.0f,0.0f,1.0f));
+	mMat=tMat*rMat;
 	
-	glLinkProgram(vfProgram);
-	checkOpenGLError();
-	glGetProgramiv(vfProgram,GL_LINK_STATUS,&linked);
-	if(linked!=1){
-		eout<<"Linking Failed!\n";
-		printProgramLog(vfProgram);
-	}
-	return vfProgram;
+	mvMat=vMat*mMat;
+	
+	glUniformMatrix4fv(mvLoc,1,GL_FALSE,glm::value_ptr(mvMat));
+	glUniformMatrix4fv(projLoc,1,GL_FALSE,glm::value_ptr(pMat));
+	
+	glBindBuffer(GL_ARRAY_BUFFER,vbo[0]);
+	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,0);
+	glEnableVertexAttribArray(0);
+	
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDrawArrays(GL_TRIANGLES,0,36);
 }
 
-void printShaderLog(GLuint shader){
-	int len=0;
-	int chWrittn=0;
-	char* log;
-	glGetShaderiv(shader,GL_INFO_LOG_LENGTH,&len);
-	if(len>0){
-		log=(char*)malloc(len);
-		glGetShaderInfoLog(shader,len,&chWrittn,log);
-		eout<<"Shader Info Log: "<<log<<'\n';
-		free(log);
-	}
+void setupVertices(){
+	float vertexPositions[108]={
+		-1.0f,1.0f,-1.0f,-1.0f,-1.0f,-1.0f,1.0f,-1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,1.0f,1.0f,-1.0f,-1.0f,1.0f,-1.0f,
+		1.0,-1.0f,-1.0f,1.0f,-1.0f,1.0f,1.0f,1.0f,-1.0f,
+		1.0f,-1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,-1.0f,
+		1.0f,-1.0f,1.0f,-1.0f,-1.0f,1.0f,1.0f,1.0f,1.0f,
+		-1.0f,-1.0f,1.0f,-1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,
+		-1.0f,-1.0f,1.0f,-1.0f,-1.0f,-1.0f,-1.0f,1.0f,1.0f,
+		-1.0f,-1.0f,-1.0f,-1.0f,1.0f,-1.0f,-1.0f,1.0f,1.0f,
+		-1.0f,-1.0f,1.0f,1.0f,-1.0f,1.0f,1.0f,-1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,1.0f,
+		-1.0f,1.0f,-1.0f,1.0f,1.0f,-1.0f,1.0f,1.0f,1.0f,
+		1.0f,1.0f,1.0f,-1.0f,1.0f,1.0f,-1.0f,1.0f,-1.0f
+	};
+	
+	glGenVertexArrays(1,vao);
+	glBindVertexArray(vao[0]);
+	glGenBuffers(numVBOs,vbo);
+	glBindBuffer(GL_ARRAY_BUFFER,vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER,sizeof(vertexPositions),vertexPositions,GL_STATIC_DRAW);
 }
-
-void printProgramLog(int prog){
-	int len=0;
-	int chWrittn=0;
-	char* log;
-	glGetProgramiv(prog,GL_INFO_LOG_LENGTH,&len);
-	if(len>0){
-		log=(char*)malloc(len);
-		glGetProgramInfoLog(prog,len,&chWrittn,log);
-		eout<<"Program Info Log: "<<log<<'\n';
-		free(log);
-	}
-}
-
-bool checkOpenGLError(){
-	bool foundError=false;
-	int glErr=glGetError();
-	while(glErr!=GL_NO_ERROR){
-		eout<<"glError: "<<glErr<<'\n';
-		foundError=true;
-		glErr=glGetError();
-	}
-	return foundError;
-}
-
-std::string readShaderSource(const char* filePath){
-	std::string content;
-	std::ifstream fileStream(filePath,ios::in);
-	string line="";
-	while(!fileStream.eof()){
-		getline(fileStream,line);
-		content.append(line+'\n');
-	}
-	fileStream.close();
-	return content;
-}
-
